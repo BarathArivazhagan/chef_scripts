d9bf30574aea226b98f6528c912ef69acdb79380
BarathArivazhagan <barathmacdec91@gmail.com>
2018-03-04 05:23:45 +0000

    added sonarqube chef

---
+++ iac_chef_sonarqube/README.md
@@ -0,0 +1,4 @@
+# iac_chef_sonarqube
+
+TODO: Enter the cookbook description here.
+
---
+++ iac_chef_sonarqube/recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: iac_chef_sonarqube
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
